3814c2bb973fc57eeff921f648baf82333e80ee1
Saksham <sakshamkumar1415@gmail.com>
2026-01-03 23:33:11 +0530

    feat: react, tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-red-500 text-4xl font-bold">KIK GMUN WORKSHOP</h1>
+    </div>
+  );
+}
+
+export default App;
